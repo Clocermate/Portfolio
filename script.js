6be4674ccd11ab4dd82735a0d29fe3cace1abb0d
Clocermate <clocermate@gmail.com>
2023-05-16 19:22:50 +0530

    Rewrite Act 2

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ window.onscroll = () => {
   header.classList.toggle("sticky", window.scrollY > 100);
 
   let menuIcon = document.querySelector("#menu-icon");
-  let navbar = document.querySelector(".mavbar");
+  let navbar = document.querySelector(".navbar");
   menuIcon.onclick = () => {
     menuIcon.classList.remove("bx-x");
     navbar.classList.remove("active");
@@ -34,22 +34,28 @@ window.onscroll = () => {
 };
 
 ScrollReveal({
-     reset: true ,
-    distance:'80px',
-duration:2000,
-delay:200
-    });
-    ScrollReveal().reveal('.home-content,.heading', { origin: 'top'});
-    ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,.contact form', { origin: 'bottom'});
-    ScrollReveal().reveal('.home-content h1,.about-img', { origin: 'left'});
-    ScrollReveal().reveal('.home-content p,.about-content', { origin: 'right'});
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+ScrollReveal().reveal(".home-content,.heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img,.services-container,.portfolio-box,.contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1,.about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p,.about-content", { origin: "right" });
 
-
-
-const typed=new Typed('.multiple-text',{
-  strings:['Frontend Developer','Backend Developer','Full Stack Developer','Video Editor'],
-  typeSpeed:100,
-  typeSpeed:100,
-  backDelay:1000,
-  loop:true
-})
+const typed = new Typed(".multiple-text", {
+  strings: [
+    "Frontend Developer",
+    "Backend Developer",
+    "Full Stack Developer",
+    "Video Editor",
+  ],
+  typeSpeed: 100,
+  typeSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
